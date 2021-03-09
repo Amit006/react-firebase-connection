5bf28ac9e404113099eca315dfb8d788b32267b8
Amit006 <amitnk19@gmail.com>
2021-03-09 19:10:17 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./style.css";
-import { googleProvider, initialDB } from "common/connection";
+import {
+  googleProvider,
+  initialDB,
+  database,
+  defaultFunction
+} from "./common/connection";
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!hkhkjh</h1>
       <p>Start editing to see some magic happen :)</p>
+      {}
     </div>
   );
 }
--- src/common/connection.js
+++ src/common/connection.js
@@ -3,7 +3,7 @@ import { firebase } from "@firebase/app";
 import "firebase/database";
 import "firebase/auth";
 
-firebase.initializeApp({
+let DB_CONFIG = {
   apiKey: "AIzaSyCu81QkqkIsKhz2LyhPdvTXpUirrDyqBtg",
   authDomain: "scocialconnect.firebaseapp.com",
   databaseURL: "https://scocialconnect-default-rtdb.firebaseio.com",
@@ -12,22 +12,44 @@ firebase.initializeApp({
   messagingSenderId: 315363602682,
   appId: "1:315363602682:web:6516cac85a98e4e9971943",
   measurementId: "G-LW1K1BQRDP"
-});
+};
 
-export const db = firebase.firestore();
+if (!firebase?.apps?.length) {
+  firebase.initializeApp(DB_CONFIG);
+}
+
+// export const db = firebase.firestore();
 export const auth = new firebase.auth();
-// export const  database = firebase.database();
+export const database = firebase.database();
 
-console.log("db: ", db);
-db.collection("users")
+database
+  .child("users")
   .get()
-  .then(querySnapshot => {
-    querySnapshot.forEach(doc => {});
+  .then(function(snapshot) {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  })
+  .catch(function(error) {
+    console.error(error);
   });
+
+// console.log("db: ", db);
+// db.collection("users")
+//   .get()
+//   .then(querySnapshot => {
+//     querySnapshot.forEach(doc => {});
+//   });
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-export default function initialDB() {
+export function defaultFunction() {
+  console.log(" Default Function ");
+}
+
+export function initialDB() {
   // Initialize Cloud Firestore through Firebase
   firebase.initializeApp({
     apiKey: "AIzaSyCu81QkqkIsKhz2LyhPdvTXpUirrDyqBtg",
